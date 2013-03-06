dca2d0647e02bf40640170428e08d3cb72584ec9
EnoahNetzach <f.castellarin@gmail.com>
2013-03-07 00:21:51 +0100

    Initial commit
    Change-Id: I968cec711b37b696c3413c725daeb2b61f9437ed
    Signed-off-by: EnoahNetzach <f.castellarin@gmail.com>

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Mar 7, 2013
+ *      Author: Enoah Netzach
+ */
+
+#include <iostream>
+
+int main()
+{
+   std::cout << "Hello World" << std::endl;
+
+   return 0;
+}
