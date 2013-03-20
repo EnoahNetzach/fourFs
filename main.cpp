599278272d551cfac53247bb1942eddfcb0c985c
EnoahNetzach <f.castellarin@gmail.com>
2013-03-20 11:31:54 +0100

    initial main file

--- main.cpp
+++ main.cpp
@@ -1,45 +1,14 @@
 /*
  * main.cpp
  *
- *  Created on: Mar 7, 2013
+ *  Created on: Mar 20, 2013
  *      Author: Enoah Netzach
  */
 
-#include <climits>
 #include <iostream>
-#include <string>
-#include <unistd.h>
 
-char m[10000],
-     * n[99],
-     * r = m,
-     * p = m + 5000,
-     ** s = n,
-     d,
-     c;
-
-int main()
+int main(int argc, char * argv[])
 {
-   std::string quine = "+[-<+]-ÿ<<<<<<<[.>]";
-   std::string result = "";
-
-   unsigned i = 0;
-   for (read(0, r, 5000); c = * r; r++)
-   {
-      c - ']' || ((d > 1 || (r = * p ? * s : (--s, r)), ! d || d--), i++),
-      c - '[' || d++ || (* ++s = r),
-      d || ( * p += c == '+',
-            * p -= c == '-',
-            p += c == '>',
-            p -= c == '<',
-            c - '.' || write(1, p, 1),
-            c - ',' || read(0, p, 1));
-
-      //std::cout << "\t" << c << " : " << p << "\t\t- " << i << std::endl;
-   };
-
-   std::cout << std::endl;
-
-   return 0;
+	return 0;
 }
 
