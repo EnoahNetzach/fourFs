8aea466260f33882ea415d6301482663fb290977
EnoahNetzach <f.castellarin@gmail.com>
2013-03-23 15:41:25 +0100

    Terrain class

---
+++ terrain.cpp
@@ -0,0 +1,65 @@
+/*
+ * terrain.cpp
+ *
+ *  Created on: Mar 23, 2013
+ *      Author: Enoah Netzach
+ */
+
+#include "terrain.h"
+
+#include <iostream>
+
+using namespace FourFs;
+
+Terrain::Terrain(unsigned width, unsigned height)
+   : m_matrix(width, height)
+{
+}
+
+Terrain::~Terrain()
+{
+}
+
+unsigned Terrain::height() const
+{
+   return m_matrix.height();
+}
+
+unsigned Terrain::width() const
+{
+   return m_matrix.width();
+}
+
+Matrix & Terrain::matrix()
+{
+   return m_matrix;
+}
+
+const Matrix & Terrain::matrix() const
+{
+   return m_matrix;
+}
+
+void Terrain::show() const
+{
+   for (unsigned y = 0; y < m_matrix.height(); y++)
+   {
+      for (unsigned x = 0; x < m_matrix.width(); x++)
+      {
+         const Pixel & pixel = m_matrix.pixelAtPosition(x, y);
+
+         char c;
+         if (pixel.isUnitsEmpty())
+         {
+            c = 'á';
+         }
+         else
+         {
+            c = 48 + pixel.nOfUnits();
+         }
+         std::cout << c << " ";
+      }
+      std::cout << "\n";
+   }
+   std::cout << std::flush;
+}
---
+++ terrain.h
@@ -0,0 +1,36 @@
+/*
+ * terrain.h
+ *
+ *  Created on: Mar 23, 2013
+ *      Author: Enoah Netzach
+ */
+
+#ifndef fourFs_TERRAIN_H_
+#define fourFs_TERRAIN_H_
+
+#include "matrix.h"
+#include "utilities.hpp"
+
+namespace FourFs
+{
+
+class Terrain
+{
+public:
+   Terrain(unsigned width, unsigned height);
+   ~Terrain();
+
+   unsigned height() const;
+   unsigned width() const;
+   Matrix & matrix();
+   const Matrix & matrix() const;
+   void show() const;
+
+protected:
+
+private:
+   Matrix m_matrix;
+};
+
+} /* namespace FourFs */
+#endif /* fourFs_TERRAIN_H_ */
