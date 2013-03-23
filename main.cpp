be79aa4e8959d286cda2e16bb3083472047b07e3
EnoahNetzach <f.castellarin@gmail.com>
2013-03-23 02:07:17 +0100

    Going on...

--- main.cpp
+++ main.cpp
@@ -19,20 +19,30 @@ int main(int argc, char * argv[])
 {
    Matrix matrix(20, 20);
 
-   Unit unit1, unit2;
+   Unit unit1, unit2, unit3;
 
    pixelsList area1 = matrix.pixelsAroundPosition(6, 14);
    pixelsList area2 = matrix.pixelsAroundPosition(15, 2);
+   pixelsList area3 = matrix.pixelsAroundPosition(8, 10);
 
    pixelsIterator it;
    for (it = area1.begin(); it != area1.end(); ++it)
    {
-//      unit1.addPixel(* it); // TODO resolve
+      unit1.addPixel(** it);
+      (* it)->addUnit(unit1);
    }
    for (it = area2.begin(); it != area2.end(); ++it)
    {
-//      unit2.addPixel(* it); // TODO resolve
+      unit2.addPixel(** it);
+      (* it)->addUnit(unit2);
    }
+   for (it = area3.begin(); it != area3.end(); ++it)
+   {
+      unit3.addPixel(** it);
+      (* it)->addUnit(unit3);
+   }
+
+   matrix.print();
 
 	return 0;
 }
