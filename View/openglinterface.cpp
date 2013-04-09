672d084e6da759e6d19c08a61fa71461ea808bde
EnoahNetzach <f.castellarin@gmail.com>
2013-04-09 23:21:22 +0200

    Some explanations about OpenGLInterface class

--- View/openglinterface.cpp
+++ View/openglinterface.cpp
@@ -16,27 +16,40 @@ using namespace view;
 OpenGLInterface::OpenGLInterface(bool time)
    : Interface_base(time, "openGL")
 {
+   // initialization and other things NOT here
+   // use initializeImpl() instead.
 }
 
 OpenGLInterface::~OpenGLInterface()
 {
-   m_runLoopThread.interrupt();
+   // garbage collector, clear all
+
+   // stops the run loop thread
+   m_runLoopThread.join();
 }
 
 void OpenGLInterface::initializeImpl()
 {
+   // resources init here!
+   // if everything is OK set m_good to true, else set it to false.
+   // If something more complex is needed, overload inherited bool good() const.
+
    m_good = true;
 }
 
 void OpenGLInterface::showImpl(sharedConstMatrix map)
 {
-   std::cout << "An openGL fabulous show here!" << std::endl;
+   // everything needed to show a new window here!
 
    m_runLoopThread = boost::thread(& OpenGLInterface::runLoop, this);
 }
 
 void OpenGLInterface::runLoop()
 {
+   // the run loop!
+   // Use boost mutex and boost interrupt points for code stability
+   // (refer to http://www.boost.org/doc/libs/1_53_0/doc/html/thread.html).
+
    while (true)
    {
       // ...
