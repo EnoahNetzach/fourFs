1c73cf1c59fe4a492d466e3f03d52164e22f2a31
Lorenzo Moro <ostrocagliostro@gmail.com>
2013-04-09 16:07:09 +0200

    Simple OpenGl window added

--- View/openglinterface.cpp
+++ View/openglinterface.cpp
@@ -3,11 +3,18 @@
  *
  *  Created on: Apr 5, 2013
  *      Author: Enoah Netzach
+ *
+ *  LIBS := -lGL -lGLU -lGLEW -lglfw
+ *  This works only with OpenGl 3.3 or above
+
  */
 
 #include "openglinterface.h"
 
 #include <iostream>
+
+#include "../Logic/map.h"
+#include "../Logic/pixel.h"
 
 using namespace fourFs;
 using namespace logic;
@@ -16,6 +23,8 @@ using namespace view;
 OpenGLInterface::OpenGLInterface(bool time)
    : Interface_base(time, "openGL")
 {
+   window_width = 1024;
+   window_height = 768;
 }
 
 OpenGLInterface::~OpenGLInterface()
@@ -24,12 +33,21 @@ OpenGLInterface::~OpenGLInterface()
 
 void OpenGLInterface::initializeImpl()
 {
-   m_good = true;
+   bool windowError = initializeWindow();
+
+   m_good = windowError;
+
 }
 
 void OpenGLInterface::showMapImpl(const logic::Map & map) const
 {
-   std::cout << "An openGL fabulous show here!" << std::endl;
+
+   //SET VBO
+
+   //SET SHADERS
+
+   OpenGlMainLoop();
+
 }
 
 void OpenGLInterface::showUnitsImpl(const logic::Map & map) const
@@ -37,4 +55,108 @@ void OpenGLInterface::showUnitsImpl(const logic::Map & map) const
    std::cout << "An openGL fabulous show here!" << std::endl;
 }
 
-//Devin sei bellissima (test)
+void OpenGLInterface::OpenGlMainLoop(void) const
+{
+   unsigned frameCount = 0;
+   double oldTime = 0.;
+
+   do
+      {
+         // Clear the screen
+         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+         //DO SOMETHING
+
+         ++frameCount;
+         oldTime = fotogramsPerSecond(frameCount, 1.0, oldTime);     //FPS counter
+
+         glfwSwapBuffers();   // Swap buffers
+
+      }  while(glfwGetKey(GLFW_KEY_ESC) != GLFW_PRESS && glfwGetWindowParam(GLFW_OPENED));
+
+      // Close OpenGL window and terminate GLFW
+      glfwTerminate();
+}
+
+bool OpenGLInterface::initializeWindow(void)
+{
+   // Initialize GLFW
+   if(!glfwInit())
+   {
+       std::cerr<<"Failed to initialize GLFW"<<std::endl;
+       return false;
+   }
+
+   glfwOpenWindowHint(GLFW_FSAA_SAMPLES, 4);                   // 4x antialiasing
+   glfwOpenWindowHint(GLFW_OPENGL_VERSION_MAJOR, 3);           // Major number of the desired minimum OpenGL version.
+   glfwOpenWindowHint(GLFW_OPENGL_VERSION_MINOR, 3);           // Minor number of the desired minimum OpenGL version.
+   glfwOpenWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);    // Disallow legacy functionality if needed (OpenGl 3.0 or above)
+   glfwOpenWindowHint(GLFW_OPENGL_PROFILE, 0);                 // Default. Let the system choose which context should implement.
+
+   // Open a window "1024x768" and create its OpenGL context
+   if(!glfwOpenWindow(window_width, window_height, 0,0,0,0, 32,0, GLFW_WINDOW))
+   {
+       std::cerr<<"Failed to open GLFW window"<<std::endl;
+
+       glfwTerminate();
+
+       return -1;
+   }
+       else
+   {
+       int major, minor, rev;
+
+       glfwGetGLVersion(&major, &minor, &rev);
+
+       std::cout<<"OpenGL version received: "<<major<<"."<<minor<<"."<<rev<<"\n"<<std::endl;
+   }
+
+   // Initialize GLEW
+   glewExperimental = true; // Needed for core profile
+
+   if (glewInit() != GLEW_OK) {
+
+       std::cerr<<"Failed to initialize GLEW"<<std::endl;
+
+       return false;
+   }
+
+   glfwSetWindowTitle(WINDOW_TITLE_DEFAULT); // Set a default Title
+
+   // Black background -> Red-Green-Blue-Alpha
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+
+   // Ensure we can capture the escape key being pressed below
+   glfwEnable(GLFW_STICKY_KEYS);
+
+   //END GLEW initialization
+
+   return true;
+}
+
+double fotogramsPerSecond(unsigned int &frameCount, double step, double oldTime){
+
+   double newTime = glfwGetTime();
+
+   if(frameCount == 0){
+      glfwSetTime(0.0);
+   }
+
+   if(std::abs(oldTime - newTime) > step)
+   {
+      unsigned int fps = frameCount/(newTime - oldTime);
+
+      std::stringstream oss;
+
+      oss << WINDOW_TITLE_DEFAULT <<" @"<< fps << " fps";
+
+      std::string newTitle (oss.str());
+
+      glfwSetWindowTitle(newTitle.c_str());
+
+      oldTime = newTime;
+      frameCount=0;
+   }
+
+   return oldTime;
+}
--- View/openglinterface.h
+++ View/openglinterface.h
@@ -9,11 +9,19 @@
 #define fourFs_OPENGLINTERFACE_H_
 
 #include "interface_base.h"
-
 #include "../utilities.hpp"
+
+#include <GL/glew.h>          // Include GLEW
+#include <GL/glfw.h>          // Include GLFW
+#include <glm/glm.hpp>        // Include GLM
+
+#include <sstream>
+#include <math.h>
 
 namespace fourFs {
 namespace view {
+
+#define WINDOW_TITLE_DEFAULT "Rob Halford?"
 
 class OpenGLInterface : virtual public Interface_base
 {
@@ -22,14 +30,20 @@ public:
    ~OpenGLInterface();
 
 protected:
+   unsigned int window_width, window_height;
+
    void initializeImpl();
+   bool initializeWindow();
    void showMapImpl(const logic::Map & map) const;
    void showUnitsImpl(const logic::Map & map) const;
+   void OpenGlMainLoop(void) const;
 
 private:
 };
 
 } /* namespace view */
 } /* namespace fourFs */
+
+double fotogramsPerSecond(unsigned int &, double, double);
 
 #endif /* fourFs_OPENGLINTERFACE_H_ */
